9e1379b2a7355b4be5f015e6ee11dcf0315681a6
saransh-dev12 <saransh.coder2006@gmail.com>
2026-07-10 21:54:45 +0530

    Conversion Operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = true
+
+
+// console.log(typeof score); 
+// console.log(typeof(score));
+
+let valueInNumber = Number(score); // convert string to number
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+let isloggedin = "saransh";
+
+let booleanIsLoggedIn = Boolean(isloggedin); // convert number to boolean
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "saransh" => true
+// "" => false 
+
+let number = 23;
+
+let someNumber = String(number); // convert number to string
+console.log(someNumber);
+console.log(typeof someNumber);
