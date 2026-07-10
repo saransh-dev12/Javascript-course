ab989b67dc65dbf9e9a13bae09b9aa020b989b28
saransh-dev12 <saransh.coder2006@gmail.com>
2026-07-10 21:08:57 +0530

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert("Hello");
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3); //code readability should be high 
+
+console.log("Saransh");
+
+let name = "Saransh";
+let age = 21;
+let isLoggedIn = false;
+
+// number = 2 to the power 53 is the range
+// bigint 
+
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> value is not defined
+// symbol ==> unique 
+
+
+//object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
