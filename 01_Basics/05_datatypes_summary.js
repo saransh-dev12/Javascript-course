c6a1bc70a7b76c2d4b351a4c8d140719b4decc10
saransh-dev12 <saransh.coder2006@gmail.com>
2026-07-13 20:00:44 +0530

    Stack & Heap

--- 01_Basics/05_datatypes_summary.js
+++ 01_Basics/05_datatypes_summary.js
@@ -42,3 +42,28 @@ console.log(typeof myObj); // object
 console.log(typeof bigNumber); // bigint
 
 
+// +++++++++++++++++++++++++++++++++++++++++++++++
+// Stack memory vs Heap memory
+
+// Stack memory is used for primitive data types. (a copy is provided)
+// Heap memory is used for non-primitive data types. (a reference is provided)
+
+let myYoutubeName = "roukinzone";
+
+let anotherName = myYoutubeName; 
+anotherName = "something";
+// copy of the value is provided
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let userOne = {
+    email: "userone@example.com",
+    upiID: "user@ybl"
+};
+
+let userTwo = userOne;
+userTwo.email = "saransh@google.com";
+console.log(userOne.email);
+console.log(userTwo.email); // reference is provided and the value is changed in both the objects.
+
