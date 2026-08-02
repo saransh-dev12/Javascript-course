484a88c1a2c4929d2a2a1552c43b8d2cc1cc5307
saransh-dev12 <saransh.coder2006@gmail.com>
2026-08-02 18:59:35 +0530

    Functions contd.

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -50,6 +50,38 @@ function logInUserMessage(username = "Sam"){
 }
 
 //console.log(logInUserMessage("Saransh "))
-console.log(logInUserMessage("Saransh")) // shows undefined
+//console.log(logInUserMessage("Saransh")) // shows undefined
 
 
+function calculateCartPrice(val1, val2,...num1){ // rest operator
+    return num1
+}
+
+//console.log(calculateCartPrice(200,300,400, 2000, 2001))
+
+const user = {
+    username: "Saransh",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200,650,500,600,77]));
