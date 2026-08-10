c235d69f06e02376b3714f61c89216659684a6b6
saransh-dev12 <saransh.coder2006@gmail.com>
2026-08-10 19:08:37 +0530

    Scopes CONTD.

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -4,15 +4,58 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log(a);
-   
+   // console.log(a);
+   function addnum(){
+
+   }
 
 } 
-for(let i = 0; i<array.length; i++){
-    console.log(array[i]);
-}
 
-console.log(a);
+
+//console.log(a);
 //console.log(b);
 //console.log(c);
 
+function one(){
+    const username = "Saransh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);  // in nested functions child functions can access parent functions variables but not vice versa
+    }
+    //console.log(website);
+    // two();
+}
+
+// one();
+
+if(true){
+    const username = "Saransh"
+    if(username === "Saransh"){
+        const website = " youtube"
+        //console.log(username + website);
+
+    }
+    // console.log(website);
+
+}
+
+// console.log(username);
+
+
+
+
+// +++++++++++++ interesting +++++++++++++
+
+console.log(addOne(5)); // 6
+function addOne(num){
+    return num + 1
+}
+
+
+
+//addTwo(5) 
+const addTwo = function(num){ // function is hold in a value so it cannot be accesed by the compiler
+    return num + 2;
+
+}
