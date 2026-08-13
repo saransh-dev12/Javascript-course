c0dfc7b01695f2d2d5acbcd0c1a914e93f5d9631
saransh-dev12 <saransh.coder2006@gmail.com>
2026-08-14 03:10:26 +0530

    Switch

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -7,19 +7,19 @@
 //         break;
 // }
 
-const month = 3;
+const month = "march";
 
 switch (month) {
-    case 1:
+    case "jan":
         console.log("January");
         break;
-    case 2:
+    case "feb":
         console.log("February");
         break;
-    case 3:
+    case "march":
         console.log("March");
         break;
-    case 4:
+    case "april":
         console.log("April");
         break;
     default:
