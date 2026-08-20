5bd9aef30a9e6e04f0a6fe9a3cd09f3db46981e5
saransh-dev12 <saransh.coder2006@gmail.com>
2026-08-20 23:59:30 +0530

    Iterations by map

---
+++ 05_Iterations/seven.js
@@ -0,0 +1,11 @@
+const myNumers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNumers.map( (num) => num + 10 )
+//console.log(newNums);
+
+
+
+// myNumers.forEach((item) => {
+//     console.log(item + 20);
+// });
+
