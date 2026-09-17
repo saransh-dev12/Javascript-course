9749fb7e97b60257b076325031cf3a05d17b853f
saransh-dev12 <saransh.coder2006@gmail.com>
2026-09-17 20:41:31 +0530

    project one revisited

--- project1/projectOne.js
+++ project1/projectOne.js
@@ -6,20 +6,25 @@ buttons.forEach(function (button){
   button.addEventListener('click', function(e){
     console.log(e)
     console.log(e.target)
-    if(e.target.id === "grey"){
+    if (e.target.id === "grey") {
       body.style.backgroundColor = e.target.id
+      
     }
-    if(e.target.id === "white"){
+    if (e.target.id === "white") {
       body.style.backgroundColor = e.target.id
+      
     }
-    if(e.target.id === "blue"){
+    if (e.target.id === "blue") {
       body.style.backgroundColor = e.target.id
+      
     }
-    if(e.target.id === "yellow"){
+    if (e.target.id === "yellow") {
       body.style.backgroundColor = e.target.id
+      
     }
-    if(e.target.id === "purple"){
+    if (e.target.id === "purple") {
       body.style.backgroundColor = e.target.id
+      
     }
   })
 })
