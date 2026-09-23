88cc74e2f3ae3c22dc7a7d610525d9b3129a5104
saransh-dev12 <saransh.coder2006@gmail.com>
2026-09-24 03:53:58 +0530

    Project two

--- project2/projectTwo.js
+++ project2/projectTwo.js
@@ -1,10 +1,15 @@
-const form = document.querySelector('form')
+const form = document.querySelector('form');
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-   const height = parseInt(document.querySelector('#height').value)
-   const weight = parseInt(document.querySelector('#weight').value)
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
 
-})
-
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid height ${height}`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid height ${weight}`;
+    }
+});
